527994c145d5448e0976e3017e31d7959d6fd00d
Moonak <afkani.mohammad@gmail.com>
2018-10-07 17:05:07 +0330

    sth was not nedde

--- diff.py
+++ diff.py
@@ -9,7 +9,7 @@ class diff:
         return round((self(x+epsilon)-self(x-epsilon))/(2*epsilon),10)
     def dd(self,x):
         epsilon = D(1e-20)
-        return round((self(x+epsilon)-2*self(x)+self(x-epsilon))/(epsilon**2),10)
+
 def f(y):
     return D(y**8)
 print(diff.d(f,10))
